0aae1b78bd7d332577db6a18e4cc047809ff93e1
bogmart <36724163+bogmart@users.noreply.github.com>
2019-07-25 11:53:37 +0300

    wr.sh: set TMPDIR

--- wr.sh
+++ wr.sh
@@ -8,7 +8,7 @@ shine_dir=/media/SSD/bogmart_workbench/p5_smart_shine
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${wind_linux}/workbench-3.3/foundation/x86-linux2/lib:${wind_linux}/lmapi-5.0/x86-linux2/lib
 
-export TMPDIR=/media/tmpfs/
+export TMPDIR=/tmp/
 
 
 ${wind_linux}/workbench-3.3/foundation/x86-linux2/bin/wtxregd restart
