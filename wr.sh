aebf308e5ddbbff4fab364822e9d39085d390cf9
bogmart <36724163+bogmart@users.noreply.github.com>
2020-08-21 18:39:33 +0300

    wr: vx + linux + git_worktree

--- wr.sh
+++ wr.sh
@@ -7,26 +7,71 @@
 #NoColo='\033[0m'
 #echo -e "${RED} !!! Please SET !! \n\n SHINE_SHARED_DIR=/media/ssd/git_hm_sources/p5_hirschmann_shared_work \n ${NoColo}"
 
-#echo "argument(s) supplied: " $1
-if [ ! -z "$1" ] 
+print_usage()
+{
+  echo "$(basename $0) {options}
+       -l = env for linux
+       -v = env for vxWork
+       -w = git worktree 
+      "
+}
+
+if [ -z "$1" ] 
 then
-  wind_linux=/media/ssd/WindRiver_work
-  shine_dir=/media/ssd/git_hm_sources/p5_smart_shine_work
-  export SHINE_SHARED_DIR=/media/ssd/git_hm_sources/p5_hirschmann_shared_work 
-else
-  wind_linux=/media/ssd/WindRiver
-  shine_dir=/media/ssd/git_hm_sources/p5_smart_shine
-  export SHINE_SHARED_DIR=/media/ssd/git_hm_sources/p5_hirschmann_shared
+  print_usage
+  exit 1
 fi
 
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${wind_linux}/workbench-3.3/foundation/x86-linux2/lib:${wind_linux}/lmapi-5.0/x86-linux2/lib
+# parse given parameters
+while getopts "lvwh " opt; do
+  case ${opt} in
+    l)
+      buildType="linux"
+      ;;
+    v)
+      buildType="vxworks"
+      ;;
+    w)
+      workSuffix="_work"
+      ;;
+    *)
+      echo ""
+      print_usage
+      exit 2
+      ;;
+  esac
+done
 
-export TMPDIR=/tmp/
+wind_linux=/media/ssd/WindRiver${workSuffix}
+shine_dir=/media/ssd/git_hm_sources/p5_smart_shine${workSuffix}
+export SHINE_SHARED_DIR=/media/ssd/git_hm_sources/p5_hirschmann_shared${workSuffix}
+export WEB_SOURCE_DIR=/media/ssd/git_hm_sources/p5_gwt_webif${workSuffix}
+export WEB_HELP_DIR=/media/ssd/git_hm_sources/p5_web_help${workSuffix}
+export LINUX_BASE_DIR=/media/ssd/git_hm_sources/p5_linux_kernel${workSuffix}
 
 
-${wind_linux}/workbench-3.3/foundation/x86-linux2/bin/wtxregd restart
+export TMPDIR=/media/ssd/TMPDIR
+export TEMP=/media/ssd/TMPDIR
+export TMP=/media/ssd/TMPDIR
 
 cd ${shine_dir}
 
-${wind_linux}/wrenv.linux -p vxworks-6.9 
+case ${buildType} in
+  "vxworks")
+    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${wind_linux}/workbench-3.3/foundation/x86-linux2/lib:${wind_linux}/lmapi-5.0/x86-linux2/lib
+    ${wind_linux}/workbench-3.3/foundation/x86-linux2/bin/wtxregd restart
+    ${wind_linux}/wrenv.linux -p vxworks-6.9 
+    ;;
+
+  "linux")
+    bash
+    ;;
+esac
+
+
+
+
+
+
+
 
